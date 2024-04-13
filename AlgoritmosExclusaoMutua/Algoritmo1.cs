06ae9831b9b9687311c06f87ac2a4c1ecfe840a6
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2024-04-13 00:15:37 +0000

    Exemplo mostra problema dessa implementacao, um processo tem um tempo mais longo na zona critica, dessa forma o outro processo fica a amior parte do tempo ocioso

--- AlgoritmosExclusaoMutua/Algoritmo1.cs
+++ AlgoritmosExclusaoMutua/Algoritmo1.cs
@@ -3,7 +3,7 @@
         private static char _vez;
         static void Main(string[] args) {
             Console.WriteLine("INICIANDO ALGORITMO 1\n\n");
-            _vez = 'A';
+            _vez = 'B';
             Thread t1 = new Thread(ProcedureA);
             Thread t2 = new Thread(ProcedureB);
             t1.Start();
@@ -15,26 +15,30 @@
         private static void ProcedureA() {
             while (true) {
                 while (_vez == 'B') {
+                    Console.WriteLine("ANDA LOGO! ESTOU ESPERANDO - PROCEDURE A");
+                    Task.Delay(300).Wait();
                     // Não faz nada
                 }
                 Console.WriteLine("=========== Seção crítica A ===========");
                 Task.Delay(5000).Wait();
                 _vez = 'B';
                 Console.WriteLine("Processamento da seção não crítica A");
-                Task.Delay(5000).Wait();
+                Task.Delay(2000).Wait();
             }
         }
 
         private static void ProcedureB() {
             while (true) {
                 while (_vez == 'A') {
+                    Console.WriteLine("ANDA LOGO! ESTOU ESPERANDO - PROCEDURE B");
+                    Task.Delay(300).Wait();
                     // Não faz nada
                 }
                 Console.WriteLine("=========== Seção crítica B ===========");
-                Task.Delay(5000).Wait();
+                Task.Delay(3000).Wait();
                 _vez = 'A';
                 Console.WriteLine("Processamento da seção não crítica B");
-                Task.Delay(5000).Wait();
+                Task.Delay(2000).Wait();
             }
         }
     }
